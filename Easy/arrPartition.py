e401df52c116685b56865221747f431ab67e21b8
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-09-29 07:38:58 +0530

    Update arrPartition.py

--- Easy/arrPartition.py
+++ Easy/arrPartition.py
@@ -20,6 +20,21 @@ class Solution:
                 newArr.append(elem)
 
         return newArr
+    
+# time O(n)
+# space O(1)
+# without retaining the order of non sero array elements
+class Solution:
+    def solve(self, A):
+        boundry = 0
+
+        for elemIndex in range(len(A)):
+            if A[elemIndex] is 0:
+                A[elemIndex] = A[boundry]
+                A[boundry] = 0
+                boundry += 1
+
+        return A
 
 sol = Solution()
 A = [2,5,-1,0,4,-2,0]
