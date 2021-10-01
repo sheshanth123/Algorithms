e98fa7de358a20e790997e1c28582d19b610dd84
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-10-01 08:04:13 +0530

    Create binarySearchWithDuplicatesLastOccurence.py

---
+++ Medium/binarySearchWithDuplicatesLastOccurence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, A, target):
+        mid = 0
+        start = 0
+        end = len(A) - 1
+
+        while(start <= end):
+            mid = start + (end-start)//2
+
+            if (A[mid] < target) or (A[mid] == target and A[start+1] == target):
+                start = mid+1
+            elif A[mid] >  target:
+                end = mid-1
+            else:
+                return mid
+        return -1
+
+sol = Solution()
+A = [1,2,2,2,2,2,2,3,4,5]
+target = 2
+output = sol.solve(A,target)
+print(output)
