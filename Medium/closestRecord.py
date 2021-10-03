1a161ebcc844fafbd47d7ef7513bb91e7185c3bf
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-10-03 20:22:05 +0530

    Create closestRecord.py

---
+++ Medium/closestRecord.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def calcNearestRecord(self, inp, target):
+        return abs(target-inp)
+
+    def solve(self, A, target):
+
+        start, end = 0, len(A)-1
+        result = end
+        while(start <= end):
+            mid = start + (end-start)//2
+
+            if A[mid] == target:
+                return A[mid]
+            elif A[mid] < target:
+                start = mid+1
+            else:
+                end = mid -1 
+            
+            if abs(target - A[mid]) < abs(target - result):
+                result = A[mid] 
+            
+
+        return result
+            
+sol = Solution()
+A = [10, 23, 30, 40, 50, 60, 70]
+output = sol.solve(A, 73)
+print(output)
