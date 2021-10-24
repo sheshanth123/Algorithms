59cd74141c07e59d8823290cc01a6da4e5c490ec
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-10-25 02:13:38 +0530

    Update searchRange.py

--- Medium/searchRange.py
+++ Medium/searchRange.py
@@ -45,3 +45,44 @@ class Solution:
             else:
                 rightIndex -= 1
         return [rngLow, rngHigh]
+    
+# using common function
+class Solution:
+
+    def findBound (self, nums, target, isFirst):
+
+        lenNums = len(nums)
+        leftIndex = 0
+        rightIndex = lenNums -1
+
+        while leftIndex <= rightIndex:
+            mid = leftIndex + (rightIndex-leftIndex)//2
+
+            if nums[mid] == target:
+                if isFirst:
+                    if mid > 0 and nums[mid-1] == target:
+                        rightIndex -= 1
+                    else:
+                        return mid
+                else:
+                    if mid < lenNums-1 and nums[mid+1] == target:
+                        leftIndex += 1
+                    else:
+                        return mid
+
+            elif nums[mid] < target:
+                leftIndex += 1
+            else:
+                rightIndex -=1
+        return -1
+
+
+    def searchRange(self, nums, target) :
+        if len(nums) == 0 or (len(nums) == 1 and nums[0] != target):
+            return [-1,-1]
+        elif len(nums) == 1:
+            return [0,0]
+        rngLow = self.findBound(nums, target, True)
+        rngHigh = self.findBound(nums, target, False)
+        return [rngLow, rngHigh]
+        
