e1f60363ab4e593c773a274f8e33791dc7f0b725
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-10-25 23:31:57 +0530

    Update peakIndexInMountainArray.py

--- Easy/peakIndexInMountainArray.py
+++ Easy/peakIndexInMountainArray.py
@@ -9,3 +9,22 @@ class Solution:
             if arr[elemIndex] > arr[elemIndex+1]:
                 return elemIndex
         return lenArr-1
+
+    
+# time -> O(logn)
+# space -> O(1)
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        lenArr = len(arr)
+        leftIndex = 0
+        rightIndex = lenArr-1
+        
+        while leftIndex <= rightIndex:
+            mid = leftIndex + (rightIndex-leftIndex)//2
+            
+            if arr[mid] < arr[mid+1]:
+                leftIndex = mid+1
+            else:
+                rightIndex = mid-1
+        return leftIndex
