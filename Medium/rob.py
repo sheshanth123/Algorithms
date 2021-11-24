29c5732da24d06a9c512a3f76d1dc1a03458d50e
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-11-25 01:05:01 +0530

    Update rob.py

--- Medium/rob.py
+++ Medium/rob.py
@@ -23,3 +23,27 @@ class Solution:
         lenNums = len(nums)
         
         return self.robHelperMethod(nums, lenNums-1, memoize)
+    
+#bottom up
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        lenNums = len(nums)
+        
+        if lenNums == 1:
+            return nums[0]
+        
+        if lenNums == 2:
+            return max(nums[0], nums[1])
+        
+        dp  =[0]* lenNums
+        
+        dp[0] = nums[0]
+        
+        dp[1] = max(nums[0], nums[1])
+        
+        for numsIndex in range(2, lenNums):
+            dp[numsIndex] = max(dp[numsIndex-1], dp[numsIndex-2]+nums[numsIndex])
+            
+        return dp[-1]
