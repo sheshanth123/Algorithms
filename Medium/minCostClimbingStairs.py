dd28e65b6dde8b1ac564086954bf523871b8153c
Sheshanth <44315878+sheshanth123@users.noreply.github.com>
2021-11-26 08:02:43 +0530

    Update minCostClimbingStairs.py

--- Medium/minCostClimbingStairs.py
+++ Medium/minCostClimbingStairs.py
@@ -1,6 +1,6 @@
 # time O(n)
 # space O(n)
-
+# bottom up approach
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
@@ -12,3 +12,20 @@ class Solution:
             dp[elemIndex] = min(dp[elemIndex-1]+ cost[elemIndex-1], \
                                dp[elemIndex-2]+ cost[elemIndex-2])
         return dp[-1]
+    
+# top down approach
+class Solution:
+    
+    def dp(self, cost, currIndex, memoize):
+        if currIndex < 2:
+            return 0      
+        if memoize.get(currIndex) is None:
+            memoize[currIndex] = min(self.dp(cost, currIndex-1, memoize)+cost[currIndex-1], \
+                                    self.dp(cost, currIndex-2, memoize)+cost[currIndex-2])          
+        return memoize[currIndex]
+    
+    
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memoize = {} 
+        top = len(cost)      
+        return self.dp(cost, top , memoize)
